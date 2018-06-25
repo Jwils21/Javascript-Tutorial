733d4f530799194ea961706b6c79c437e7d34688
Jonathan Wilson <jon.wilsond@gmail.com>
2018-06-25 15:18:30 -0400

    Update V1

--- js/counter.js
+++ js/counter.js
@@ -1,18 +1,46 @@
 "option strict"
 
-//var inputctrl = document.getElementById("value");
 var theNumber = 0;
+var inputctrl = null;
+
+function loaded() {
+    //capture the input box
+    inputctrl = document.getElementById("value");
+    //store number to 0
+    display();
+    inputctrl.style.textAlign="center";
+}
+
 //inputctrl.value = theNumber;
 
 function add() {
-    var inputctrl = document.getElementById("value");
     theNumber++;
-    inputctrl.value = theNumber;
+    display();
 }
 
 function sub() {
-    var inputctrl = document.getElementById("value");
     theNumber--;
-    inputctrl.value = theNumber;
+    display();
 }
 
+function display(){
+    //store the new number value
+    inputctrl.value = theNumber;
+    //figure out whether the number is even or odd
+    var isEven    
+    ((inputctrl.value%2)==0) ? isEven = true : isEven = false;
+
+    //color the number accordingly
+    if (isEven) {
+        inputctrl.style.color = "red";
+    } else {
+        inputctrl.style.color = "black";
+    }
+    //bold if divisible by 3
+    var isDiv3 = ((inputctrl.value%3)==0);
+    inputctrl.style.fontWeight = (isDiv3) ? "bold": "normal";
+    //italicize if divisible by 7
+    var isDiv7 = ((inputctrl.value%7)==0);
+    inputctrl.style.fontStyle = (isDiv7) ? "italic": "normal";
+
+}
